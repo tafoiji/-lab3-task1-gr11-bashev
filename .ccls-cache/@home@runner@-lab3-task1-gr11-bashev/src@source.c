1b1bb26d8bc64635212b7d8154407b9f649f50b9
tafoiji <126417382+tafoiji@users.noreply.github.com>
2024-03-04 13:04:44 +0000

    added function

--- .ccls-cache/@home@runner@-lab3-task1-gr11-bashev/src@source.c
+++ .ccls-cache/@home@runner@-lab3-task1-gr11-bashev/src@source.c
@@ -1,8 +1,18 @@
-/* hello.c */
+/* source.c */
 #include <stdio.h>
 
-void main (void)
+int getSum10(int n)
 {
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
+  int sum;
+  for (sum = 0; n != 0; n /= 10) 
+  {
+    sum += n % 10;
+  }
+
+  return sum;
+}
+
+int main(int argc, char *argv[]) 
+{
+  printf("%d \n", getSum10(9999));
 }
